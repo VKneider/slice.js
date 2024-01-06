3b91fb6210216f0fedd57ada3dc714a08a258f19
Victor Kneider <victorkneider@gmail.com>
2024-01-05 20:32:16 -0400

    feat: add method to register component styles, created style html tag to store components individual css

--- Slice/Components/Structural/StylesManager/StylesManager.js
+++ Slice/Components/Structural/StylesManager/StylesManager.js
@@ -3,6 +3,10 @@ import ThemeManager from "./ThemeManager/ThemeManager.js"
 
 export default class StylesManager {
     constructor() {
+      this.componentStyles = document.createElement("style");
+      this.componentStyles.id = "slice-component-styles";
+      document.head.appendChild(this.componentStyles);
+      
       this.customStylesManager = new CustomStylesManager();
       this.themeManager = new ThemeManager();
     }  
@@ -16,6 +20,18 @@ export default class StylesManager {
     setTheme(themeName){
       this.themeManager.setTheme(themeName);
     }
+
+    //add a method that will add css as text to the componentStyles element
+    appendComponentStyles(cssText) {
+      this.componentStyles.appendChild(document.createTextNode(cssText));
+    }
+
+    registerComponentStyles(componentName, cssText) {
+      slice.controller.requestedStyles.add(componentName);
+      this.appendComponentStyles(cssText);
+    }
+
+
 
 
   }
