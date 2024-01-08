6cb772d6031ad6352b6c0b0b9c66213a8605d206
Victor Kneider <victorkneider@gmail.com>
2024-01-08 16:10:56 -0400

    Add button to change theme and update theme

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-const tester = await slice.build("Tester", { id: "tester", sliceId: "tester", subject: "Este es el subject de un componente Tester", description: "Este es el description de un componente Tester" })
+const tester = await slice.build("Tester", {
+    id: "tester",
+    sliceId: "tester",
+    subject: "Este es el subject de un componente Tester",
+    description: "Este es el description de un componente Tester"
+})
 
-const tester2 = await slice.build("Tester", { id: "tester2", sliceId: "tester2", subject: "HOLA profe" })
+const tester2 = await slice.build("Tester", {
+    id: "tester2",
+    sliceId: "tester2",
+    subject: "HOLA profe"
+})
 
 
+let theme = "Slice";
+
+
+const boton = document.getElementById("boton");
+boton.innerHTML = "Cambiar al tema Dark";
+boton.addEventListener("click", async () => {
+    
+    if(theme==="Slice"){
+        await slice.stylesManager.setTheme("Dark");
+        theme = "Dark";
+        boton.innerHTML = "Cambiar al tema Slice";
+    }else {
+
+        await slice.stylesManager.setTheme("Slice");
+        theme = "Slice";
+        boton.innerHTML = "Cambiar al tema Dark";
+    }
+})
 document.body.appendChild(tester);
 document.body.appendChild(tester2);
 
-document.body.appendChild(slice.debugger) 
