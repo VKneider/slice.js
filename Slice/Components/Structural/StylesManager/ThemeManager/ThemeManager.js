ceb840b15b024584a8ef7ed5935469a0f7b94a9b
Victor Kneider <victorkneider@gmail.com>
2024-01-08 16:09:41 -0400

    refactor: ThemeManager class to improve theme handling

--- Slice/Components/Structural/StylesManager/ThemeManager/ThemeManager.js
+++ Slice/Components/Structural/StylesManager/ThemeManager/ThemeManager.js
@@ -1,50 +1,39 @@
 export default class ThemeManager {
-    constructor() {
-      this.registeredThemes = new Map();
-      this.currentThemeLink = null;
-    }
-  
-    // Registrar un tema con su nombre
-    registerTheme(themeName, isDefault = false) {
-      this.registeredThemes.set(themeName, isDefault);
-    }
-  
-    applyTheme(themeName) {
-        if (!this.isThemeRegistered(themeName)) {
-            this.removeCurrentTheme();
-            const themeCSSURL = `${slice.paths.themes}/${themeName}.css`; // Usar slice.paths.themes
-            this.loadThemeCSS(themeCSSURL);        
-        }
-      }
-    
-  
-    removeCurrentTheme() {
-      if (this.currentThemeLink) {
-        this.currentThemeLink.parentNode.removeChild(this.currentThemeLink);
-      }
-    }
-  
-    loadThemeCSS(themeCSSURL) {
-      const themeLink = document.createElement('link');
-      themeLink.rel = 'stylesheet';
-      themeLink.href = themeCSSURL;
-      themeLink.setAttribute('data-theme', 'true');
-      document.head.appendChild(themeLink);
-      this.currentThemeLink = themeLink;
-    }
-  
-    isThemeRegistered(themeName) {
-      return this.registeredThemes.has(themeName);
-    }
-  
-   
+  constructor() {
+    this.themeStyles = new Map();
+    this.currentThemeStyle = null;
   }
 
-  /*
+  async applyTheme(themeName) {
+      if (!this.themeStyles.has(themeName)) {
+          this.removeCurrentTheme();
+          await this.loadThemeCSS(themeName);        
+      } else {
+          this.setThemeStyle(themeName);
+      }
+  }
+  
+  removeCurrentTheme() {
+    if (this.currentThemeStyle) {
+      this.currentThemeStyle.parentNode.removeChild(this.currentThemeStyle);
+    }
+  }
 
-    la filosofia va a ser que el usuario utilice el tema slice por default o setee un tema personalizado, luego que simplemente 
-    utilice componentes que ya tendrán el tema puesto.
+  async loadThemeCSS( themeName) {
 
-    el tema slice por default va a ser el tema que se carga por default en el constructor de ThemeManager, y va a ser el tema que se carga
+    const themeCss = await slice.controller.fetchText(themeName, "theme");
+    this.themeStyles.set(themeName, themeCss);
+    this.setThemeStyle(themeName);
+    
+  }
 
-  */
+  setThemeStyle(themeName) {
+      const themeStyle = document.createElement('style');
+      themeStyle.textContent = this.themeStyles.get(themeName);
+      document.head.appendChild(themeStyle);
+      this.currentThemeStyle = themeStyle;
+  }
+
+  
+}
+
