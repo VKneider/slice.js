3695d46d500e68adce0fe9d0b9c052a93be4520d
Julio Graterol <juliograterolb@gmail.com>
2024-01-13 01:43:55 -0400

    slice-input component

--- index.js
+++ index.js
@@ -1,34 +1,63 @@
-const tester2 = await slice.build("Tester", {
-    id: "tester2",
-    sliceId: "tester2",
-    subject: "HOLA profe"
-})
+const tester = await slice.build("Tester", {
+  id: "tester",
+  sliceId: "tester",
+  subject: "Hola",
+  description: "Saludo",
+});
 
-const tester3 = await slice.build("Tester2", {
-    id: "tester3",
-    sliceId: "tester3",
-    subject: "HOLA profe",
-    children:true
-})
+const name1 = await slice.build("Input", {
+  id: "nombre",
+  placeholder: "Name",
+  required: true,
+});
+const lastname = await slice.build("Input", {
+  placeholder: "Last Name",
+});
+const password = await slice.build("Input", {
+  placeholder: "Password",
+  type: "password",
+  required: true,
+  secret: true,
+});
+
+const form = document.getElementById("form");
+
+form.appendChild(name1);
+form.appendChild(lastname);
+form.appendChild(password);
+
+const boton = document.getElementById("boton");
+
+boton.innerHTML = "Cambiar al tema Light";
 
 let theme = "Slice";
 
-
-const boton = document.getElementById("boton");
-boton.innerHTML = "Cambiar al tema Dark";
 boton.addEventListener("click", async () => {
-    
-    if(theme==="Slice"){
-        await slice.stylesManager.setTheme("Dark");
-        theme = "Dark";
-        boton.innerHTML = "Cambiar al tema Slice";
-    }else {
+  if (theme === "Slice") {
+    await slice.stylesManager.setTheme("Light");
+    theme = "Light";
+    boton.innerHTML = "Cambiar al tema Dark";
+  } else if (theme === "Light") {
+    await slice.stylesManager.setTheme("Dark");
+    theme = "Dark";
+    boton.innerHTML = "Cambiar al tema Slice";
+  } else if (theme === "Dark") {
+    await slice.stylesManager.setTheme("Slice");
+    theme = "Slice";
+    boton.innerHTML = "Cambiar al tema Light";
+  }
+  // console.log(theme);
+});
 
-        await slice.stylesManager.setTheme("Slice");
-        theme = "Slice";
-        boton.innerHTML = "Cambiar al tema Dark";
-    }
-})
-document.body.appendChild(tester3);
+const botonName = document.getElementById("botonName");
+const botonClear = document.getElementById("botonClear");
 
-
+botonName.addEventListener("click", () => {
+  console.log(name1.getValue());
+  console.log(lastname.getValue());
+});
+botonClear.addEventListener("click", () => {
+  name1.clear();
+  lastname.clear();
+  password.clear();
+});
