df88ffec75009401ee1ec0a334f5d5fd9dd5eb03
Julio Graterol <juliograterolb@gmail.com>
2024-01-15 13:41:50 -0400

    input added has regex

--- Slice/Components/Visual/Input/Input.js
+++ Slice/Components/Visual/Input/Input.js
@@ -5,6 +5,13 @@ export default class Input extends HTMLElement {
     this.placeholder = this.querySelector(".slice_input_placeholder");
     this.slice_input = this.querySelector(".slice_input");
     this.input = this.querySelector(".input_area");
+    this.hasRegex = false;
+    this.regex;
+
+    if (props.regex) {
+      this.hasRegex = true;
+      this.regexConditions(props.regex);
+    }
 
     for (const prop in props) {
       this.setAttribute(prop, props[prop]);
@@ -32,6 +39,7 @@ export default class Input extends HTMLElement {
               this.placeholder.classList.remove("slice_input_value");
             }
           });
+
           break;
 
         case "value":
@@ -47,24 +55,20 @@ export default class Input extends HTMLElement {
           if (newValue === "true") {
             this.input.addEventListener("input", () => {
               if (this.input.value === "") {
-                this.placeholder.classList.add("placeholder_required");
-                this.input.classList.add("input_required");
+                this.triggerError();
               } else {
-                this.placeholder.classList.remove("placeholder_required");
-                this.input.classList.remove("input_required");
+                this.triggerSuccess();
               }
             });
           }
           break;
 
         case "type":
-          if (newValue === "password") {
-            this.input.type = "password";
-          }
+          this.input.type = newValue;
           break;
 
         case "secret":
-          if (newValue === "true") {
+          if (newValue === "true" && this.input.type === "password") {
             const revealButton = document.createElement("div");
             revealButton.classList.add("eye");
 
@@ -91,10 +95,68 @@ export default class Input extends HTMLElement {
     }
   }
 
-  getValue() {
-    return this.input.value;
+  regexConditions(conditions) {
+    const {
+      explicit = "",
+      minLength = 0,
+      maxLength = "",
+      minMinusc = 0,
+      maxMinusc = "",
+      minMayusc = 0,
+      maxMayusc = "",
+      minNumber = 0,
+      maxNumber = "",
+      minSymbol = 0,
+      maxSymbol = "",
+    } = conditions;
+
+    let regexPattern = "";
+
+    if (explicit !== "") {
+      regexPattern = explicit;
+    } else {
+      regexPattern =
+        `^(?=.*[a-z]{${minMinusc},${maxMinusc}})` +
+        `(?=.*[A-Z]{${minMayusc},${maxMayusc}})` +
+        `(?=.*\\d{${minNumber},${maxNumber}})` +
+        `(?=.*[\\W_]{${minSymbol},${maxSymbol}})` +
+        `.{${minLength},${maxLength}}$`;
+    }
+
+    this.regex = new RegExp(regexPattern);
   }
 
+  triggerSuccess() {
+    this.placeholder.classList.remove("placeholder_required");
+    this.input.classList.remove("input_required");
+  }
+
+  triggerError() {
+    this.slice_input.classList.add("error");
+    this.placeholder.classList.add("placeholder_required");
+    this.input.classList.add("input_required");
+    setTimeout(() => {
+      this.slice_input.classList.remove("error");
+    }, 500);
+  }
+
+  getValue() {
+    if (this.hasRegex && !this.regex.test(this.input.value)) {
+      this.triggerError();
+      return `This is not a valid ${this.input.type}`;
+    }
+
+    return this.input.value;
+  }
+  setValue(string) {
+    this.input.value = string;
+  }
+  getPlacegolder() {
+    return this.input.placeholder;
+  }
+  setPlaceholder(string) {
+    this.input.placeholder = string;
+  }
   clear() {
     if (this.input.value !== "") {
       this.input.value = "";
