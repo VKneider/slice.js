c0b6ca46bf9e34b94ee8257d036dbc7925647fba
Victor Kneider <victorkneider@gmail.com>
2024-04-02 22:42:00 -0400

    Refactor ThemeManager class to improve theme loading and apply logic

--- Slice/Components/Structural/StylesManager/ThemeManager/ThemeManager.js
+++ Slice/Components/Structural/StylesManager/ThemeManager/ThemeManager.js
@@ -1,38 +1,34 @@
 export default class ThemeManager {
   constructor() {
     this.themeStyles = new Map();
-    this.currentThemeStyle = null;
+    this.currentTheme = null;
+    this.$themeStyle = document.createElement('style');
+    document.head.appendChild(this.$themeStyle);
   }
 
   async applyTheme(themeName) {
-      if (!this.themeStyles.has(themeName)) {
-          this.removeCurrentTheme();
-          await this.loadThemeCSS(themeName);        
-      } else {
-          this.setThemeStyle(themeName);
-      }
-  }
-  
-  removeCurrentTheme() {
-    if (this.currentThemeStyle) {
-      this.currentThemeStyle.parentNode.removeChild(this.currentThemeStyle);
+    if (!this.themeStyles.has(themeName)) {
+      await this.loadThemeCSS(themeName);
+    } else {
+      this.setThemeStyle(themeName);
     }
   }
 
-  async loadThemeCSS( themeName) {
-
+  async loadThemeCSS(themeName) {
     const themeCss = await slice.controller.fetchText(themeName, "theme");
     this.themeStyles.set(themeName, themeCss);
+    this.removeCurrentTheme(); // Elimina el estilo del tema anterior si existe
     this.setThemeStyle(themeName);
-    
+  }
+
+  removeCurrentTheme() {
+    if (this.currentTheme) {
+      this.$themeStyle.textContent = '';
+    }
   }
 
   setThemeStyle(themeName) {
-      const themeStyle = document.createElement('style');
-      themeStyle.textContent = this.themeStyles.get(themeName);
-      document.head.appendChild(themeStyle);
-      this.currentThemeStyle = themeStyle;
+    this.$themeStyle.textContent = this.themeStyles.get(themeName);
+    this.currentTheme = themeName;
   }
-
-  
 }
