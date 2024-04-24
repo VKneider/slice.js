205b0b8d5d495cbd207e5f8e40d3ccf265237073
Julio Graterol <juliograterolb@gmail.com>
2024-04-24 18:31:47 -0400

    button documentation

--- src/Components/Visual/ButtonDocumentation/ButtonDocumentation.js
+++ src/Components/Visual/ButtonDocumentation/ButtonDocumentation.js
@@ -8,26 +8,48 @@ export default class ButtonDocumentation extends HTMLElement {
   }
 
   async init() {
-    await this.createButton({ value: "Slice Button" });
-    await this.createButton({
+    const grid = await slice.build("Grid", { columns: 2, rows: 1 });
+    grid.classList.add("buttons");
+
+    const simpleButton = await this.createButton(null, {});
+    const myButton = await this.createButton(null, {
+      value: "Slice Button",
+    });
+
+    await grid.setItem(simpleButton);
+    await grid.setItem(myButton);
+
+    this.querySelector(".myButton").appendChild(grid);
+
+    await this.createButton(this.querySelector(".colorButton"), {
       value: "Color Button",
       customColor: { color: "black", button: "red" },
     });
-    const clickButton = await this.createButton({
-      value: "Click",
-      onClickCallback: () => {
-        clickButton.value = "Clicked";
-      },
-    });
+    const clickButton = await this.createButton(
+      this.querySelector(".onClick"),
+      {
+        value: "Click",
+        onClickCallback:
+          '() => { if (clickButton.value === "Click") { clickButton.value = "Clicked"; } else { clickButton.value = "Click"; }}',
+        click: () => {
+          if (clickButton.value === "Click") {
+            clickButton.value = "Clicked";
+          } else {
+            clickButton.value = "Click";
+          }
+        },
+      }
+    );
   }
 
-  async createButton(buttonProps) {
-    const myButton = await slice.build("Button", buttonProps);
-
+  async createButton(appendTo, buttonProps) {
     const formattedValue = JSON.stringify(buttonProps, null, 2).replace(
       /,(\s*)/g,
       ",\n$1"
     );
+
+    buttonProps.onClickCallback = buttonProps.click;
+    const myButton = await slice.build("Button", buttonProps);
 
     const componentCode = await slice.build("CodeVisualizer", {
       value: `const myButton = await slice.build("Button", 
@@ -35,8 +57,15 @@ ${formattedValue});`,
       language: "javascript",
     });
 
-    this.appendChild(myButton);
-    this.appendChild(componentCode);
+    const div = document.createElement("div");
+    div.appendChild(myButton);
+    div.appendChild(componentCode);
+
+    if (appendTo) {
+      appendTo.appendChild(div);
+    }
+
+    return div;
   }
 }
 
