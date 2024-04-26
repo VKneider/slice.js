6c42639fddbfedc5f501abb2756fc3354179f57c
Julio Graterol <juliograterolb@gmail.com>
2024-04-25 21:10:42 -0400

    landing changes

--- src/Components/Visual/Landing/Landing.js
+++ src/Components/Visual/Landing/Landing.js
@@ -7,9 +7,20 @@ export default class Landing extends HTMLElement {
     this.debuggerProps = [];
   }
 
-  init() {}
-
-  
+  async init() {
+    let logoSize = "500px";
+    if (window.screen.width <= 770) {
+      logoSize = "300px";
+    }
+    const sliceLogo = await slice.build("Icon", {
+      name: "sliceJs",
+      size: logoSize,
+      color: "var(--font-primary-color)",
+      iconStyle: "filled",
+    });
+    sliceLogo.classList.add("sliceLogo");
+    this.querySelector(".intro").appendChild(sliceLogo);
+  }
 }
 
 customElements.define("slice-landing", Landing);
