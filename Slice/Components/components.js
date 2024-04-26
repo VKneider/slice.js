94164ca7b9715a9c7099d3bd252ce74a6b21e460
Julio Graterol <juliograterolb@gmail.com>
2024-04-25 20:06:35 -0400

    documentation page

--- Slice/Components/components.js
+++ Slice/Components/components.js
@@ -25,7 +25,9 @@ const components = {
   "ButtonDocumentation": "UserVisual",
   "CardDocumentation": "UserVisual",
   "CodeVisualizer": "UserVisual",
+  "Documentation": "UserVisual",
   "InputDocumentation": "UserVisual",
   "MainMenu": "UserVisual",
-  "MyLayout": "UserVisual"
+  "MyLayout": "UserVisual",
+  "MyNavBar": "UserVisual"
 }; export default components;
