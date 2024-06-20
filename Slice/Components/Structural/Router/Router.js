c05b7073760792be6aa603df5160b9feb3ea06ef
Victor Kneider <victorkneider@gmail.com>
2024-06-20 12:20:04 -0400

    feat: created Router structural component for routing components in applications, added routes object example

---
+++ Slice/Components/Structural/Router/Router.js
@@ -0,0 +1,57 @@
+import routes from "./routes.js";
+
+export default class Router {
+  constructor() {
+    this.routes = routes;
+    this.loadInitialRoute();
+    window.addEventListener('popstate', this.onRouteChange.bind(this));
+  }
+
+  init(){
+    document.addEventListener('DOMContentLoaded', () => {
+      document.querySelectorAll('a[data-route]').forEach(anchor => {
+        anchor.addEventListener('click', function (event) {
+          event.preventDefault();
+          slice.router.navigate(this.getAttribute('href'));
+        });
+      });
+    });
+  }
+
+  async onRouteChange() {
+    const path = window.location.pathname;
+    const route = this.matchRoute(path);
+    if (route) {
+      await this.handleRoute(route);
+    }
+  }
+
+  async navigate(path) {
+    window.history.pushState({}, path, window.location.origin + path);
+    await this.onRouteChange();
+  }
+
+  async handleRoute(route) {
+    if (route.component) {
+      const component = await slice.build(route.component, {});
+      const targetElement = route.target ? document.querySelector(route.target) : document.querySelector('#app');
+      targetElement.innerHTML = '';
+      targetElement.appendChild(component);
+    } else if (route.callback) {
+      route.callback(route.path);
+    }
+  }
+
+  async loadInitialRoute() {
+    const path = window.location.pathname;
+    const route = this.matchRoute(path);
+    if (route) {
+      await this.handleRoute(route);
+    }
+  }
+
+  matchRoute(path) {
+    return this.routes.find(r => r.path === path);
+  }
+}
+
---
+++ Slice/Components/Structural/Router/routes.js
@@ -0,0 +1,9 @@
+const routes = [
+  { path: '/', component: "LandingPage" },
+  { path: '/Documentation', component: "Documentation"},
+  { path: '/Documentation/Card', component: "CardDocumentation"},
+];
+
+
+
+export default routes;
