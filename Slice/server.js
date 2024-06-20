486942d79e183501d4674b41e204147a1a329a8a
Victor Kneider <victorkneider@gmail.com>
2024-06-20 12:21:16 -0400

    feat: create server to dispatch same index.html for every route requested by the users

---
+++ Slice/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+
+// Servir archivos estáticos desde la carpeta 'App'
+app.use(express.static(path.join(__dirname, '..', 'src')));
+
+app.use('/slice', express.static(path.join(__dirname,'..', 'Slice')));
+// Servir archivos estáticos desde la carpeta 'Slice'
+
+
+// Ruta para servir el index.html desde la carpeta 'App'
+app.get('*', (req, res) => {
+  const filePath = path.join(__dirname, '..', 'src', 'App', 'index.html');
+  res.sendFile(filePath);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
