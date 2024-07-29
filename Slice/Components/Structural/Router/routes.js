fbb6840dd3e754f6bff57bb7a423259713c443aa
Victor Kneider <victorkneider@gmail.com>
2024-07-29 02:07:48 -0400

    feat: added component reusability for routing in routes JSON

--- Slice/Components/Structural/Router/routes.js
+++ Slice/Components/Structural/Router/routes.js
@@ -1,9 +1,12 @@
 const routes = [
-  { path: '/', component: "LandingPage" },
-  { path: '/Playground', component: 'Playground' },
+  { path: '/', component: "Test1", reuse: true },
+  { path: '/2', component: "Test2", reuse: true },
+  { path: '/3', component: "Test3", reuse: true},
+  { path: '/Playground', component: 'Playground', reuse: true },
   { path: '/Documentation', component: "DocumentationPage" },
+  { path: '/Documentation/*/', component: 'DocumentationPage' },
   { path: '/404', component: 'NotFound' },
-  { path: '/Documentation/*', component: 'DocumentationPage' }
+  { path: '/rutica/que/obviamente/existe/xd/xd/', component: 'DocumentationPage' },
 
 ];
 
