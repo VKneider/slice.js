22a1ff829ed737d4b3edeb047be8c01c2e1076d6
Victor Kneider <victorkneider@gmail.com>
2024-08-01 02:41:56 -0400

    feat: add children routes support to routes.js json

--- Slice/Components/Structural/Router/routes.js
+++ Slice/Components/Structural/Router/routes.js
@@ -1,13 +1,27 @@
 const routes = [
   { path: '/', component: "Test1", reuse: true },
   { path: '/2', component: "Test2", reuse: true },
-  { path: '/3', component: "Test3", reuse: true},
+  {
+    path: '/3', component: "Test3", reuse: true, children: [
+      {
+        path: "/info",
+        component: "ProductInfo"
+      },
+      {
+        path: "/list",
+        component: "ProductList"
+      },
+      {
+        path: "/search",
+        component: "ProductSearch"
+      }
+    ]
+  },
   { path: '/Playground', component: 'Playground', reuse: true },
   { path: '/Documentation', component: "DocumentationPage" },
   { path: '/Documentation/*/', component: 'DocumentationPage' },
   { path: '/404', component: 'NotFound' },
   { path: '/rutica/que/obviamente/existe/xd/xd/', component: 'DocumentationPage' },
-
 ];
 
 
