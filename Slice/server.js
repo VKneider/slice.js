47b0f0c488d531fcd5edc56f8bcb24c8834d4cdb
Victor Kneider <victorkneider@gmail.com>
2024-08-01 03:28:53 -0400

    chore: Update import path for Slice component

--- Slice/server.js
+++ Slice/server.js
@@ -16,7 +16,7 @@ app.get('/test', (req, res) => {
   res.send(`${__dirname} , ruta static: ${path.join(__dirname, '..', 'src')} y la ruta final para el index.html: ${path.join(__dirname, '..', 'src', 'App', 'index.html')}` )
 });
 
-//app.use('/Slice', express.static(path.join(__dirname,'..', 'Slice')));
+app.use('/Slice', express.static(path.join(__dirname,'..', 'Slice')));
 // Servir archivos estáticos desde la carpeta 'Slice'
 
 // Ruta para servir el index.html desde la carpeta 'App'
