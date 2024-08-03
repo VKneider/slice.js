8824ed92e3879f69ff5356544fa9669b4f9b4b57
Victor Kneider <victorkneider@gmail.com>
2024-08-03 00:15:44 -0400

    chore: update routes to normal slice page

--- Slice/Components/Structural/Router/routes.js
+++ Slice/Components/Structural/Router/routes.js
@@ -1,6 +1,5 @@
 const routes = [
-  { path: '/', component: "Test1", reuse: true },
-  { path: '/2', component: "Test2", reuse: true },
+  { path: '/', component: "LandingPage", reuse: true },
   {
     path: '/3', component: "Test3", reuse: true, children: [
       {
@@ -18,10 +17,10 @@ const routes = [
     ]
   },
   { path: '/Playground', component: 'Playground', reuse: true },
-  { path: '/Documentation', component: "DocumentationPage" },
+  { path: '/Documentation', component: "DocumentationPage", reuse: true },
   { path: '/Documentation/*/', component: 'DocumentationPage' },
   { path: '/404', component: 'NotFound' },
-  { path: '/rutica/que/obviamente/existe/xd/xd/', component: 'DocumentationPage' },
+
 ];
 
 
