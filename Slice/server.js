b0b33fe0371efbe778dbe2e9d4ee1d09f854cce8
Victor Kneider <victorkneider@gmail.com>
2024-08-07 15:59:54 -0400

    chore: Update server.js to include the full URL in the console log

--- Slice/server.js
+++ Slice/server.js
@@ -27,7 +27,7 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}, http://localhost:${PORT}`);
 });
 
 export default app;
