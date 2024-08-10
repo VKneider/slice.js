ec96c5ef63f39e08548503b2a7c77bd39ac60ead
Victor Kneider <victorkneider@gmail.com>
2024-08-10 18:46:22 -0400

    chore: Update static file serving path in server.js

--- src/Slice/server.js
+++ src/Slice/server.js
@@ -11,11 +11,16 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, '..', '..','src')));
 
+console.log(`__dirname: ${__dirname}`);
+console.log(`path.join(__dirname, '..', '..','src'): ${path.join(__dirname, '..', '..','src')}`);
+
 app.get('*', (req, res) => {
    console.log(`Requested URL: ${req.url}`)
    const filePath = path.join(__dirname, '..', 'App', 'index.html');
    res.sendFile(filePath);
 });
+
+
 
 app.listen(PORT, () => {
    console.log(`Server is running on port ${PORT}, http://localhost:${PORT}`);
