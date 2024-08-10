d7ac7b27b99bd63814319df3394612b9a25c6835
Victor Kneider <victorkneider@gmail.com>
2024-08-10 18:39:43 -0400

    chore: Update static file serving path in server.js

--- src/Slice/server.js
+++ src/Slice/server.js
@@ -9,20 +9,8 @@ const __dirname = dirname(__filename);
 const app = express();
 const PORT = 3000;
 
-// Servir archivos estáticos desde la carpeta 'Slice'
-app.use('/Slice/', (req, res, next) => {
-   console.log(`Middleware '/Slice' ejecutado. Ruta solicitada: ${req.url}`);
-   next();
-});
-/*
-app.use((req, res, next) => {
-   console.log(`Middleware base ejecutado. Ruta solicitada: ${req.url}`);
-   next();
-});
-*/
 app.use(express.static(path.join(__dirname, '..', '..','src')));
 
-// Ruta para servir el index.html desde la carpeta 'App'
 app.get('*', (req, res) => {
    console.log(`Requested URL: ${req.url}`)
    const filePath = path.join(__dirname, '..', 'App', 'index.html');
