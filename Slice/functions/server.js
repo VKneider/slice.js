8da56d862ca19f6654c899350c4a8a8ae3199db4
Victor Kneider <victorkneider@gmail.com>
2024-08-11 11:00:14 -0400

    refactor: Update server.js to use ServerlessHttp for better scalability

--- Slice/functions/server.js
+++ Slice/functions/server.js
@@ -1,11 +1,7 @@
 import express from 'express';
 import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import ServerlessHttp from 'serverless-http';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = 3000;
