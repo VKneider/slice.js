fd49460f3ea3085d566bb5f1ae0060fbd27d6660
Victor Kneider <victorkneider@gmail.com>
2024-08-13 21:56:15 -0400

    refactor: build method handles promise.all

--- Slice/Slice.js
+++ Slice/Slice.js
@@ -25,118 +25,119 @@ export default class Slice {
 
    async build(componentName, props = {}) {
       if (!componentName) {
-         this.logger.logError('Slice', null, `Component name is required to build a component`);
-         return null;
+          this.logger.logError('Slice', null, `Component name is required to build a component`);
+          return null;
       }
-
+  
       if (typeof componentName !== 'string') {
-         this.logger.logError('Slice', null, `Component name must be a string`);
-         return null;
+          this.logger.logError('Slice', null, `Component name must be a string`);
+          return null;
       }
-
+  
       if (!this.controller.componentCategories.has(componentName)) {
-         this.logger.logError('Slice', null, `Component ${componentName} not found in components.js file`);
-         return null;
+          this.logger.logError('Slice', null, `Component ${componentName} not found in components.js file`);
+          return null;
       }
-
+  
       let componentCategory = this.controller.componentCategories.get(componentName);
-
+  
       if (componentCategory === 'Structural') {
-         this.logger.logError(
-            'Slice',
-            null,
-            `Component ${componentName} is a Structural component and cannot be built`
-         );
-         return null;
+          this.logger.logError(
+              'Slice',
+              null,
+              `Component ${componentName} is a Structural component and cannot be built`
+          );
+          return null;
       }
-
+  
       let componentBasePath;
-
+  
       if (componentCategory.includes('User')) {
-         componentCategory = componentCategory.replace('User', '');
-         componentBasePath = this.paths.userComponents;
+          componentCategory = componentCategory.replace('User', '');
+          componentBasePath = this.paths.userComponents;
       } else {
-         componentBasePath = this.paths.components;
+          componentBasePath = this.paths.components;
       }
-
+  
       const isVisual = componentCategory === 'Visual';
       let modulePath = `${componentBasePath}/${componentCategory}/${componentName}/${componentName}.js`;
-
-      // Load template if not loaded previously and component category is Visual
-      if (!this.controller.templates.has(componentName) && isVisual) {
-         try {
-            const html = await this.controller.fetchText(componentName, 'html', componentBasePath, componentCategory);
-            const template = document.createElement('template');
-            template.innerHTML = html;
-            this.controller.templates.set(componentName, template);
-            this.logger.logInfo('Slice', `Template ${componentName} loaded`);
-         } catch (error) {
-            console.log(error);
-            this.logger.logError('Slice', `Error loading template ${componentName}`, error);
-         }
-      }
-
-      //Load class if not loaded previously
-      if (!this.controller.classes.has(componentName)) {
-         try {
-            const ComponentClass = await this.getClass(modulePath);
-            this.controller.classes.set(componentName, ComponentClass);
-            this.logger.logInfo('Slice', `Class ${componentName} loaded`);
-         } catch (error) {
-            console.log(error);
-            this.logger.logError('Slice', `Error loading class ${modulePath}`, error);
-         }
-      }
-
-      //Load css if not loaded previously and component category is Visual
-      if (!this.controller.requestedStyles.has(componentName) && isVisual) {
-         try {
-            const css = await this.controller.fetchText(componentName, 'css', componentBasePath, componentCategory);
-            this.stylesManager.registerComponentStyles(componentName, css);
-            this.logger.logInfo('Slice', `CSS ${componentName} loaded`);
-         } catch (error) {
-            console.log(error);
-            this.logger.logError('Slice', `Error loading css ${componentName}`, error);
-         }
-      }
-
-      //Create instance
+  
+      // Load template, class, and CSS concurrently if needed
       try {
-         let componentIds = {};
-         if (props.id) componentIds.id = props.id;
-         if (props.sliceId) componentIds.sliceId = props.sliceId;
-
-         delete props.id;
-         delete props.sliceId;
-
-         const ComponentClass = this.controller.classes.get(componentName);
-         const componentInstance = new ComponentClass(props);
-
-         if (componentIds.id && isVisual) componentInstance.id = componentIds.id;
-         if (componentIds.sliceId) componentInstance.sliceId = componentIds.sliceId;
-
-         if (!this.controller.verifyComponentIds(componentInstance)) {
-            this.logger.logError('Slice', `Error registering instance ${componentName} ${componentInstance.sliceId}`);
-            return null;
-         }
-
-         if (sliceConfig.debugger.enabled && componentCategory === 'Visual') {
-            this.debugger.attachDebugMode(componentInstance);
-         }
-
-         //if the component has a method called init, call it
-         if (componentInstance.init) await componentInstance.init();
-
-         this.controller.registerComponent(componentInstance);
-         this.controller.registerComponentsRecursively(componentInstance);
-
-         this.logger.logInfo('Slice', `Instance ${componentInstance.sliceId} created`);
-         return componentInstance;
+          const loadTemplate = isVisual && !this.controller.templates.has(componentName)
+              ? this.controller.fetchText(componentName, 'html', componentBasePath, componentCategory)
+              : Promise.resolve(null);
+  
+          const loadClass = !this.controller.classes.has(componentName)
+              ? this.getClass(modulePath)
+              : Promise.resolve(null);
+  
+          const loadCSS = isVisual && !this.controller.requestedStyles.has(componentName)
+              ? this.controller.fetchText(componentName, 'css', componentBasePath, componentCategory)
+              : Promise.resolve(null);
+  
+          const [html, ComponentClass, css] = await Promise.all([loadTemplate, loadClass, loadCSS]);
+  
+          if (html) {
+              const template = document.createElement('template');
+              template.innerHTML = html;
+              this.controller.templates.set(componentName, template);
+              this.logger.logInfo('Slice', `Template ${componentName} loaded`);
+          }
+  
+          if (ComponentClass) {
+              this.controller.classes.set(componentName, ComponentClass);
+              this.logger.logInfo('Slice', `Class ${componentName} loaded`);
+          }
+  
+          if (css) {
+              this.stylesManager.registerComponentStyles(componentName, css);
+              this.logger.logInfo('Slice', `CSS ${componentName} loaded`);
+          }
       } catch (error) {
-         console.log(error);
-         this.logger.logError('Slice', `Error creating instance ${componentName}`, error);
+          console.log(error);
+          this.logger.logError('Slice', `Error loading resources for ${componentName}`, error);
+          return null;
       }
-   }
+  
+      // Create instance
+      try {
+          let componentIds = {};
+          if (props.id) componentIds.id = props.id;
+          if (props.sliceId) componentIds.sliceId = props.sliceId;
+  
+          delete props.id;
+          delete props.sliceId;
+  
+          const ComponentClass = this.controller.classes.get(componentName);
+          const componentInstance = new ComponentClass(props);
+  
+          if (componentIds.id && isVisual) componentInstance.id = componentIds.id;
+          if (componentIds.sliceId) componentInstance.sliceId = componentIds.sliceId;
+  
+          if (!this.controller.verifyComponentIds(componentInstance)) {
+              this.logger.logError('Slice', `Error registering instance ${componentName} ${componentInstance.sliceId}`);
+              return null;
+          }
+  
+          if (sliceConfig.debugger.enabled && componentCategory === 'Visual') {
+              this.debugger.attachDebugMode(componentInstance);
+          }
+  
+          if (componentInstance.init) await componentInstance.init();
+  
+          this.controller.registerComponent(componentInstance);
+          this.controller.registerComponentsRecursively(componentInstance);
+  
+          this.logger.logInfo('Slice', `Instance ${componentInstance.sliceId} created`);
+          return componentInstance;
+      } catch (error) {
+          console.log(error);
+          this.logger.logError('Slice', `Error creating instance ${componentName}`, error);
+          return null;
+      }
+  }
+  
 
    async setTheme(themeName) {
       await this.stylesManager.themeManager.applyTheme(themeName);
