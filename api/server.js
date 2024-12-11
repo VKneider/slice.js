0a0eb88bf6788cbe68166b35c0fab7a7b17ee75c
Victor Kneider <victorkneider@gmail.com>
2024-12-10 22:49:04 -0400

    refactor: reorganize server file structure and update static file serving paths; add Vercel configuration to .gitignore

--- api/server.js
+++ api/server.js
@@ -10,16 +10,16 @@ const app = express();
 const PORT = 3000;
 
 // Servir archivos estáticos desde la carpeta 'Slice'
-app.use('/Slice/', express.static(path.join(__dirname, '..', '..', 'Slice')));
+app.use('/Slice/', express.static(path.join(__dirname, 'Slice')));
 // Servir archivos estáticos desde la carpeta 'App'
-app.use(express.static(path.join(__dirname, '..', 'src')));
+app.use(express.static(path.join(__dirname, 'src')));
 
 
 
 // Ruta para servir el index.html desde la carpeta 'App'
 app.get('*', (req, res) => {
    console.log('requesting index.html', req.url);
-   const filePath = path.join(__dirname, '..','..', 'src', 'App', 'index.html');
+   const filePath = path.join(__dirname, 'src', 'App', 'index.html');
    res.sendFile(filePath);
 });
 
