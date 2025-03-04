0a63a7304aa27ca97b4fbc2b4940e3b8111475a7
Victor Kneider <victorkneider@gmail.com>
2025-03-04 14:00:16 -0400

    refactor: update sliceConfig import path and streamline static file serving logic

--- api/index.js
+++ api/index.js
@@ -5,21 +5,25 @@ import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-import sliceConfig from '../Slice/sliceConfig.json' assert { type: 'json' };
+import sliceConfig from '../src/sliceConfig.json' assert { type: 'json' };
 
 
 const app = express();
 const PORT = 3001;
 
+const isProduction = sliceConfig.production === true;
+const folderDeployed = isProduction ? 'dist' : 'src';
+
 // Servir archivos estáticos desde la carpeta 'Slice'
 app.use('/Slice/', express.static(path.join(__dirname,'..','Slice')));
 // Servir archivos estáticos desde la carpeta 'App'
+
+
 
 app.get('/testing1', (req, res) => {
    res.send(` Actual route in server: __dirname: ${__dirname} __filename: ${__filename} - checking if file exists: ${path.join(__dirname, '..', 'src','App', 'index.html')}`);
 });
 
-let folderDeployed = sliceConfig.production === true ? 'dist' : 'src';
 
 app.use(express.static(path.join(__dirname,'..', folderDeployed)));
 
@@ -27,6 +31,10 @@ app.use(express.static(path.join(__dirname,'..', folderDeployed)));
 app.get('/testing2', (req, res) => {
    res.send(` Actual route in server: __dirname: ${__dirname} __filename: ${__filename} - checking if file exists: ${path.join(__dirname, '..', 'src','App', 'index.html')}`);
 });
+
+if(isProduction){
+   
+}
 
 
 // Ruta para servir el index.html desde la carpeta 'App'
