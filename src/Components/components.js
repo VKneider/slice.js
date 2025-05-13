a74fcc7f381410251f76d9a1412b71062eb539cb
Victor Kneider <victorkneider@gmail.com>
2025-05-12 23:10:34 -0400

    feat: add SliceStylesDocumentation component with styles and documentation; update components registry

--- src/Components/components.js
+++ src/Components/components.js
@@ -13,6 +13,7 @@ const components = {
   "MyNavigation": "AppComponents",
   "Playground": "AppComponents",
   "RoutingDocumentation": "AppComponents",
+  "SliceStylesDocumentation": "AppComponents",
   "SliceTeamCard": "AppComponents",
   "SwitchDocumentation": "AppComponents",
   "TheBuildMethod": "AppComponents",
